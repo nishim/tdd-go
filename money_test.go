1ecc2bd020e28455d4de11d2368601e2338f4378
Yuji Nishimura <nishimura.yuji@liclab.com>
2019-08-05 22:08:35 +0900

    TDD P.14

--- money_test.go
+++ money_test.go
@@ -4,16 +4,16 @@ import "testing"
 
 func TestMultiplication(t *testing.T) {
 	cases := []struct {
-		dollar   int
 		times    int
 		expected int
 	}{
-		{dollar: 5, times: 2, expected: 10},
+		{times: 2, expected: 10},
+		{times: 3, expected: 15},
 	}
 
+	five := Dollar{5}
 	for _, c := range cases {
-		d := Dollar{c.dollar}
-		d.Times(c.times)
+		d := five.Times(c.times)
 		if d.Amount != c.expected {
 			t.Errorf("expected: %d, actual: %d", c.expected, d.Amount)
 		}
