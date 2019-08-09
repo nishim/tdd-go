790c4487c0f3b96de09675b21a4a692d1d1e17e7
Yuji Nishimura <nishimura.yuji@liclab.com>
2019-08-09 21:48:13 +0900

    TDD P.79

--- money_test.go
+++ money_test.go
@@ -28,8 +28,6 @@ func TestEquality(t *testing.T) {
 	}{
 		{a: NewDollar(5), b: NewDollar(5), expected: true},
 		{a: NewDollar(5), b: NewDollar(6), expected: false},
-		{a: NewFranc(5), b: NewFranc(5), expected: true},
-		{a: NewFranc(5), b: NewFranc(6), expected: false},
 		{a: NewDollar(5), b: NewFranc(5), expected: false},
 	}
 
@@ -37,24 +35,6 @@ func TestEquality(t *testing.T) {
 		b := Equals(c.a, c.b)
 		if b != c.expected {
 			t.Errorf("expected: %v, actual: %v, a:%v, b:%v", c.expected, b, c.a, c.b)
-		}
-	}
-}
-
-func TestFrancMultiplication(t *testing.T) {
-	cases := []struct {
-		times    int
-		expected Money
-	}{
-		{times: 2, expected: NewFranc(10)},
-		{times: 3, expected: NewFranc(15)},
-	}
-
-	five := NewFranc(5)
-	for _, c := range cases {
-		d := five.Times(c.times)
-		if d != c.expected {
-			t.Errorf("expected: %v, actual: %v", c.expected, d)
 		}
 	}
 }
